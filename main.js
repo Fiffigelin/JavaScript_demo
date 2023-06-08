e54275f53e46988cf7ad2b0cb86e1b790408761b
Elin Nyman <elin.nyman87@gmail.com>
2023-06-08 13:47:48 +0200

    array with cars

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ window.addEventListener("DOMContentLoaded", main);
 
 /**The state for our program */
 
-// const listOfCarBrands = ["Volvo", "Wolksvagen", "Toyota", "BMW", "Corvette"]
+const listOfCarBrands = ["Volvo", "Wolksvagen", "Toyota", "BMW", "Corvette"]
 // const removeLastElementButton = document.getElementById('removeButton');
 
-// function main() {
-//     renderCarList();
+function main() {
+    renderCarList();
 //     removeLastElementButton.addEventListener("click", removeLastElement);
-// }
+}
 
-// function renderCarList() {
-//     const ul = document.querySelector('ul');
+function renderCarList() {
+    const ul = document.querySelector('ul');
 
-//     for (const carBrand of listOfCarBrands){
-//         const li = document.createElement('li');
-//         li.textContent = carBrand;
-//         ul.append(li);
-//     }
-// }
+    for (const carBrand of listOfCarBrands){
+        const li = document.createElement('li');
+        li.textContent = carBrand;
+        ul.append(li);
+    }
+}
 
 // function removeLastElement() {
 //     listOfCarBrands.pop();

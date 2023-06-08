d0beacf08cadac40720ffd1e78f06b7e1459af6a
Elin Nyman <elin.nyman87@gmail.com>
2023-06-08 13:38:18 +0200

    Index and main created

---
+++ main.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", main);
+
+/**The state for our program */
+
+// const listOfCarBrands = ["Volvo", "Wolksvagen", "Toyota", "BMW", "Corvette"]
+// const removeLastElementButton = document.getElementById('removeButton');
+
+// function main() {
+//     renderCarList();
+//     removeLastElementButton.addEventListener("click", removeLastElement);
+// }
+
+// function renderCarList() {
+//     const ul = document.querySelector('ul');
+
+//     for (const carBrand of listOfCarBrands){
+//         const li = document.createElement('li');
+//         li.textContent = carBrand;
+//         ul.append(li);
+//     }
+// }
+
+// function removeLastElement() {
+//     listOfCarBrands.pop();
+//     console.log(listOfCarBrands);
+//     renderCarList(); // Update the displayed list
+// }
